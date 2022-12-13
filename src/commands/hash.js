f4f475fcf08918f50a69a6e58e49066967339d5f
maks-rafalko <b0rn@list.ru>
2022-12-13 11:40:33 +0100

    refactor: extract validation of folder to separate method in `hash` command

--- src/commands/hash.js
+++ src/commands/hash.js
@@ -1,10 +1,9 @@
 import { createHash } from 'node:crypto';
-import { readFile, stat } from 'node:fs/promises';
-import { isAbsolute, join } from 'node:path';
+import { readFile } from 'node:fs/promises';
 import { validateCommandLine } from '../commandLineValidator.js';
 import { OperationFailedError } from "../OperationFailedError.js";
 import { displayMessage } from '../io.js';
-import { normalizeToAbsolutePath } from '../fsFunctions.js';
+import { folderExists, normalizeToAbsolutePath } from '../fsFunctions.js';
 
 const sha256AsHex = (content) => createHash('sha256').update(content).digest('hex');
 
@@ -13,15 +12,10 @@ export const hash = async (executionContext, parsedCommandLine) => {
 
     const targetPath = parsedCommandLine.arguments[0];
 
-    let resultPath = normalizeToAbsolutePath(executionContext.currentDir, targetPath);
+    const resultPath = normalizeToAbsolutePath(executionContext.currentDir, targetPath);
 
     try {
-        const stats = await stat(resultPath);
-
-        if (stats.isDirectory()) {
-            // todo discuss how to write it in a better way
-            throw new OperationFailedError();
-        }
+        await assertPathIsNotDirectory(resultPath);
 
         const fileBuffer = await readFile(resultPath);
 
@@ -29,6 +23,14 @@ export const hash = async (executionContext, parsedCommandLine) => {
 
         displayMessage(`SHA-256 of file "${resultPath}": ${hex}`);
     } catch (error) {
+        throw new OperationFailedError();
+    }
+}
+
+const assertPathIsNotDirectory = async (path) => {
+    const isFolderExists = await folderExists(path);
+
+    if (isFolderExists) {
         throw new OperationFailedError();
     }
 }
